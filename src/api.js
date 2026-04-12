3335ce616155c70df08efd4caf814a48d0e21828
Gustavo Maximiliano <gustavomaximiliano1@gmail.com>
2026-04-12 08:10:49 -0600

    Change API base URL to production server

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://animeweb-5aoz.onrender.com/api' });
 
 // Este "interceptor" pega el token automáticamente en cada petición si existe
 API.interceptors.request.use((req) => {
